98ea7fca6119255304120c2d0e7fb1099aa5eb7e
DAshley <954347696@qq.com>
2017-05-23 11:27:36 +0800

    test

---
+++ app/src/main/java/com/huabiao/aoiin/HuaBiaoApp.java
@@ -0,0 +1,15 @@
+package com.huabiao.aoiin;
+
+import com.ywy.mylibs.base.BaseApplication;
+
+/**
+ * Created by PC on 2017/5/23.
+ */
+
+public class HuaBiaoApp extends BaseApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
