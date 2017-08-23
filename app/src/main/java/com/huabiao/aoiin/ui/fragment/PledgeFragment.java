e1bac59b325d7c10b1f0d297ed570a6901d8b516
ya <2041609459@qq.com>
2017-08-23 15:00:39 +0800

    增加商标质押页面(无数据)

---
+++ app/src/main/java/com/huabiao/aoiin/ui/fragment/PledgeFragment.java
@@ -0,0 +1,32 @@
+package com.huabiao.aoiin.ui.fragment;
+
+import android.os.Bundle;
+
+import com.huabiao.aoiin.R;
+import com.ywy.mylibs.base.BaseFragment;
+import com.ywy.mylibs.base.BasePresenter;
+
+/**
+ * @author 杨丽亚.
+ * @PackageName com.huabiao.aoiin.ui.fragment
+ * @date 2017-08-23 14:58
+ * @description 商标质押页面
+ */
+public class PledgeFragment extends BaseFragment {
+
+
+    @Override
+    public void bindView(Bundle savedInstanceState) {
+
+    }
+
+    @Override
+    public int getContentLayout() {
+        return R.layout.pledge_layout;
+    }
+
+    @Override
+    public BasePresenter getPresenter() {
+        return null;
+    }
+}
